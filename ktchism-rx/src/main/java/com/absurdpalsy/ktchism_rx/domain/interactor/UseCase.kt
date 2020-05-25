01f36df024e8001153a5138cf05f008429278c4c
andreyrussprint <andreyrussprint@gmail.com>
2020-05-25 15:32:08 +0700

    Drop ktchism-rx

--- ktchism-rx/src/main/java/com/absurdpalsy/ktchism_rx/domain/interactor/UseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.absurdpalsy.ktchism_rx.domain.interactor
-
-abstract class UseCase<out Result, in Params> {
-
-    abstract operator fun invoke(params: Params): Result
-
-    class None
-}
