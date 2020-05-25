bad99c7be49ab8fd4b9d6295610453e1967f4ae3
andreyrussprint <andreyrussprint@gmail.com>
2020-05-25 15:35:25 +0700

    Failure

---
+++ ktchism-core/src/main/java/ktchism/exception/Failure.kt
@@ -0,0 +1,11 @@
+package ktchism.exception
+
+sealed class Failure {
+    object NetworkConnection : Failure()
+    object NotFound : Failure()
+    object Unauthorized : Failure()
+    object DataStoreError : Failure()
+    object UnexpectedError : Failure()
+
+    abstract class FeatureFailure : Failure()
+}
