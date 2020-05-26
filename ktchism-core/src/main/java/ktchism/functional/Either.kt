3ceca9bbc60feee52ac5e3e31d8bb51e1bbd911b
andreyrussprint <andreyrussprint@gmail.com>
2020-05-26 10:36:46 +0700

    Either

---
+++ ktchism-core/src/main/java/ktchism/functional/Either.kt
@@ -0,0 +1,114 @@
+package ktchism.functional
+
+import ktchism.functional.Either.Companion.right
+import ktchism.functional.Either.Left
+import ktchism.functional.Either.Right
+
+/**
+ * Represents a value of one of two possible types (a disjoint union).
+ * Instances of [Either] are either an instance of [Left] or [Right].
+ * FP Convention dictates that [Left] is used for "failure"
+ * and [Right] is used for "success".
+ */
+sealed class Either<out L, out R> {
+
+    /**
+     * Represents the left side of [Either] class which by convention is a "Failure".
+     */
+    data class Left<out L>(val value: L) : Either<L, Nothing>()
+
+    /**
+     * Represents the right side of [Either] class which by convention is a "Success".
+     */
+    data class Right<out R>(val value: R) : Either<Nothing, R>()
+
+    /**
+     * Returns true if this is a [Right], false otherwise.
+     */
+    val isRight: Boolean
+        get() = this is Right<R>
+
+    /**
+     * Returns true if this is a [Left], false otherwise.
+     */
+    val isLeft: Boolean
+        get() = this is Left<L>
+
+    companion object {
+        /**
+         * Creates a [Left] type.
+         */
+        fun <L> left(value: L): Either<L, Nothing> = Left(value)
+
+        /**
+         * Creates a [Right] type.
+         */
+        fun <R> right(value: R): Either<Nothing, R> = Right(value)
+    }
+
+    /**
+     * Applies [ifLeft] if this is a [Left] or [ifRight] if this is a [Right].
+     *
+     * @param ifLeft the function to apply if this is a [Left]
+     * @param ifRight the function to apply if this is a [Right]
+     * @return the results of applying the function
+     */
+    inline fun <T> fold(ifLeft: (L) -> T, ifRight: (R) -> T): T =
+        when (this) {
+            is Left -> ifLeft(value)
+            is Right -> ifRight(value)
+        }
+
+    /**
+     * Returns `false` if [Left] or returns the result of the application of
+     * the given predicate to the [Right] value.
+     */
+    fun exists(predicate: (R) -> Boolean): Boolean =
+        fold({ false }, { predicate(it) })
+}
+
+/**
+ * Composes 2 functions
+ */
+fun <A, B, C> ((A) -> B).c(f: (B) -> C): (A) -> C = {
+    f(this(it))
+}
+
+/**
+ * Right-biased flatMap().
+ *
+ * The given function is applied if this is a [Right].
+ */
+fun <T, L, R> Either<L, R>.flatMap(fn: (R) -> Either<L, T>): Either<L, T> =
+    when (this) {
+        is Left -> this
+        is Right -> fn(value)
+    }
+
+/**
+ * The given function is applied if this is a [Right].
+ */
+fun <T, L, R> Either<L, R>.map(fn: (R) -> T): Either<L, T> =
+    this.flatMap(fn.c(::right))
+
+/**
+ * The given function is applied if this is a [Left].
+ */
+fun <T, L, R> Either<L, R>.mapLeft(fn: (L) -> T): Either<T, R> =
+    fold({ Left(fn(it)) }, { Right(it) })
+
+/**
+ * Map over [Left] and [Right] of this [Either].
+ */
+fun <T, E, L, R> Either<L, R>.bimap(fnL: (L) -> E, fnR: (R) -> T): Either<E, T> =
+    fold({ Left(fnL(it)) }, { Right(fnR(it)) })
+
+/**
+ * Returns the value from this [Right] or the given argument if this is a [Left].
+ */
+fun <L, R> Either<L, R>.getOrElse(defaultValue: R): R =
+    when (this) {
+        is Left -> defaultValue
+        is Right -> value
+    }
+
---
+++ ktchism-core/src/test/java/ktchism/functional/EitherTest.kt
@@ -0,0 +1,205 @@
+package ktchism.functional
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
+import ktchism.UnitTest
+import ktchism.exception.Failure
+import org.junit.Assert.*
+import org.junit.Test
+
+class EitherTest : UnitTest() {
+
+    @Test
+    fun `Either Left - return correct type`() {
+        val value = "Failure"
+        val result = Either.left(value)
+
+        assertTrue(result.isLeft)
+        assertFalse(result.isRight)
+        result.fold(
+            ifLeft = { assertEquals(value, it) },
+            ifRight = { fail() }
+        )
+    }
+
+    @Test
+    fun `Either Right - return correct type`() {
+        val value = "Success"
+        val result = Either.right(value)
+
+        assertTrue(result.isRight)
+        assertFalse(result.isLeft)
+        result.fold(
+            ifLeft = { fail() },
+            ifRight = { assertEquals(value, it) }
+        )
+    }
+
+    @Test
+    fun `Either fold - if Left - invoke first argument`() {
+        val ifLeft = mock<(String) -> Unit>()
+        val ifRight = mock<(String) -> Unit>()
+
+        val value = "Failure"
+        val either = Either.left(value)
+
+        either.fold(ifLeft, ifRight)
+
+        verify(ifLeft).invoke(value)
+        verifyZeroInteractions(ifRight)
+    }
+
+    @Test
+    fun `Either fold - if Right - invoke second argument`() {
+        val ifLeft = mock<(String) -> Unit>()
+        val ifRight = mock<(String) -> Unit>()
+
+        val value = "Success"
+        val either = Either.right(value)
+
+        either.fold(ifLeft, ifRight)
+
+        verify(ifRight).invoke(value)
+        verifyZeroInteractions(ifLeft)
+    }
+
+    @Test
+    fun `Either exists - if Left - return false`() {
+        val either = Either.left("Failure")
+
+        val predicate: (Int) -> Boolean = { true }
+
+        val result = either.exists(predicate)
+        assertFalse(result)
+    }
+
+    @Test
+    fun `Either exists - if Right - apply predicate`() {
+        val either = Either.right(5)
+
+        val predicate: (Int) -> Boolean = { it > 0 }
+
+        val result = either.exists(predicate)
+        assertTrue(result)
+    }
+
+    @Test
+    fun `Either flatMap - if Left - return unchanged`() {
+        val fn = mock<(Int) -> Either<Int, String>>()
+
+        val value = -1
+        val either: Either<Int, Int> = Either.left(value)
+
+        val result = either.flatMap(fn)
+
+        verifyZeroInteractions(fn)
+        assertEquals(either, result)
+    }
+
+    @Test
+    fun `Either flatMap - if Right - apply function`() {
+        val expectedResult = Either.right(30)
+        val either = Either.right(6)
+
+        val result = either.flatMap { Either.right(it * 5) }
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Either map - if Left - return unchanged`() {
+        val fn: (Int) -> String = mock()
+
+        val either = Either.left(-1)
+
+        val result = either.map(fn)
+
+        verifyZeroInteractions(fn)
+        assertEquals(either, result)
+    }
+
+    @Test
+    fun `Either map - if Right - apply function`() {
+        val fn: (Int) -> String = { it.toString() }
+
+        val either = Either.right(5)
+
+        val expectedResult = Either.right("5")
+        val result = either.map(fn)
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Either mapLeft - if Left - apply function`() {
+        val fn: (Int) -> String = { it.toString() }
+
+        val either = Either.left(1)
+
+        val expectedResult = Either.left("1")
+        val result = either.mapLeft(fn)
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Either mapLeft - if Right - return unchanged`() {
+        val fn: (Int) -> String = mock()
+
+        val either = Either.right(1)
+
+        val result = either.mapLeft(fn)
+
+        verifyZeroInteractions(fn)
+        assertEquals(either, result)
+    }
+
+    @Test
+    fun `Either bimap - if Left - apply left function`() {
+        val fnL: (Int) -> String = { it.toString() }
+        val fnR: (String) -> Int = mock()
+
+        val either: Either<Int, String> = Either.left(5)
+
+        val expectedResult: Either<String, Int> = Either.left("5")
+        val result = either.bimap(fnL, fnR)
+
+        verifyZeroInteractions(fnR)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Either bimap - if Right - apply right function`() {
+        val fnL: (Int) -> String = mock()
+        val fnR: (String) -> Int = { it.toInt() }
+
+        val either: Either<Int, String> = Either.right("5")
+
+        val expectedResult: Either<String, Int> = Either.right(5)
+        val result = either.bimap(fnL, fnR)
+
+        verifyZeroInteractions(fnL)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Either getOrElse - if Left - return default value`() {
+        val either: Either<Int, String> = Either.left(5)
+        val defaultValue = "Default"
+
+        val result = either.getOrElse(defaultValue)
+
+        assertEquals(defaultValue, result)
+    }
+
+    @Test
+    fun `Either getOrElse - if Right - return value`() {
+        val either: Either<Int, Int> = Either.right(5)
+        val defaultValue = 3
+
+        val result = either.getOrElse(defaultValue)
+
+        assertEquals(5, result)
+    }
+}
