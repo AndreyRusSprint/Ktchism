b383082989509412ac5ca9a4e84e1f6b0790d510
andreyrussprint <andreyrussprint@gmail.com>
2020-05-27 11:20:32 +0700

    RxEither

---
+++ ktchism-rx/src/main/java/ktchism/rx/RxEither.kt
@@ -0,0 +1,27 @@
+@file:Suppress("USELESS_CAST")
+
+package ktchism.rx
+
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.Single
+import ktchism.exception.Failure
+import ktchism.functional.Either
+
+fun <T, E : Failure> Observable<T>.toEither(
+    exceptionMapper: (Throwable) -> E
+): Observable<Either<E, T>> = this
+    .map { Either.right(it) as Either<E, T> }
+    .onErrorReturn { Either.left(exceptionMapper(it)) }
+
+fun <T, E : Failure> Single<T>.toEither(
+    exceptionMapper: (Throwable) -> E
+): Single<Either<E, T>> = this
+    .map { Either.right(it) as Either<E, T> }
+    .onErrorReturn { Either.left(exceptionMapper(it)) }
+
+fun <T, E : Failure> Flowable<T>.toEither(
+    exceptionMapper: (Throwable) -> E
+): Flowable<Either<E, T>> = this
+    .map { Either.right(it) as Either<E, T> }
+    .onErrorReturn { Either.left(exceptionMapper(it)) }
