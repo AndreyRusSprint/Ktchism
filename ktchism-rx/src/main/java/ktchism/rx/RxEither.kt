3d684a05b5e33d3d4b12a1659598948b0ab4aa5c
andreyrussprint <andreyrussprint@gmail.com>
2020-06-04 15:23:03 +0700

    RxEither. ExceptionMapper support

--- ktchism-rx/src/main/java/ktchism/rx/RxEither.kt
+++ ktchism-rx/src/main/java/ktchism/rx/RxEither.kt
@@ -8,6 +8,7 @@ import io.reactivex.Observable
 import io.reactivex.Single
 import ktchism.core.exception.Failure
 import ktchism.core.functional.Either
+import ktchism.core.mapper.ExceptionMapper
 
 fun <T> Observable<T>.toEither(
     exceptionMapper: (Throwable) -> Failure
@@ -15,11 +16,23 @@ fun <T> Observable<T>.toEither(
     .map { Either.right(it) as Either<Failure, T> }
     .onErrorReturn { Either.left(exceptionMapper(it)) }
 
+fun <T> Observable<T>.toEither(
+    exceptionMapper: ExceptionMapper
+): Observable<Either<Failure, T>> = this
+    .map { Either.right(it) as Either<Failure, T> }
+    .onErrorReturn { Either.left(exceptionMapper.transform(it)) }
+
 fun <T> Single<T>.toEither(
     exceptionMapper: (Throwable) -> Failure
 ): Single<Either<Failure, T>> = this
     .map { Either.right(it) as Either<Failure, T> }
     .onErrorReturn { Either.left(exceptionMapper(it)) }
+
+fun <T> Single<T>.toEither(
+    exceptionMapper: ExceptionMapper
+): Single<Either<Failure, T>> = this
+    .map { Either.right(it) as Either<Failure, T> }
+    .onErrorReturn { Either.left(exceptionMapper.transform(it)) }
 
 fun <T> Flowable<T>.toEither(
     exceptionMapper: (Throwable) -> Failure
@@ -27,8 +40,20 @@ fun <T> Flowable<T>.toEither(
     .map { Either.right(it) as Either<Failure, T> }
     .onErrorReturn { Either.left(exceptionMapper(it)) }
 
+fun <T> Flowable<T>.toEither(
+    exceptionMapper: ExceptionMapper
+): Flowable<Either<Failure, T>> = this
+    .map { Either.right(it) as Either<Failure, T> }
+    .onErrorReturn { Either.left(exceptionMapper.transform(it)) }
+
 fun Completable.toEither(
     exceptionMapper: (Throwable) -> Failure
+): Single<Either<Failure, Unit>> = this
+    .toSingleDefault(Unit)
+    .toEither(exceptionMapper)
+
+fun Completable.toEither(
+    exceptionMapper: ExceptionMapper
 ): Single<Either<Failure, Unit>> = this
     .toSingleDefault(Unit)
     .toEither(exceptionMapper)
--- ktchism-rx/src/test/java/ktchism/rx/RxEitherTest.kt
+++ ktchism-rx/src/test/java/ktchism/rx/RxEitherTest.kt
@@ -7,6 +7,7 @@ import io.reactivex.Observable
 import io.reactivex.Single
 import ktchism.core.exception.Failure
 import ktchism.core.functional.Either
+import ktchism.core.mapper.ExceptionMapper
 import org.junit.Test
 
 class RxEitherTest {
@@ -14,7 +15,7 @@ class RxEitherTest {
     private val error = mock<Throwable>()
 
     @Test
-    fun `Observable to Either - if success - return Right`() {
+    fun `Observable to Either - if success - return Right (lambda mapper)`() {
         val observable = Observable.just(3)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -29,7 +30,20 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Observable to Either - if failure - return Left`() {
+    fun `Observable to Either - if success - return Right (base mapper)`() {
+        val observable = Observable.just(3)
+        val exceptionMapper = object : ExceptionMapper {}
+
+        observable.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Right<Int> }
+            .dispose()
+    }
+
+    @Test
+    fun `Observable to Either - if failure - return Left (lambda mapper)`() {
         val observable = Observable.error<Int>(error)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -44,7 +58,20 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Single to Either - if success - return Right`() {
+    fun `Observable to Either - if failure - return Left (base mapper)`() {
+        val observable = Observable.error<Int>(error)
+        val exceptionMapper = object : ExceptionMapper {}
+
+        observable.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Left<Failure> }
+            .dispose()
+    }
+
+    @Test
+    fun `Single to Either - if success - return Right (lambda mapper)`() {
         val single = Single.just(3)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -59,7 +86,20 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Single to Either - if failure - return Left`() {
+    fun `Single to Either - if success - return Right (base mapper)`() {
+        val single = Single.just(3)
+        val exceptionMapper = object : ExceptionMapper {}
+
+        single.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Right<Int> }
+            .dispose()
+    }
+
+    @Test
+    fun `Single to Either - if failure - return Left (lambda mapper)`() {
         val single = Single.error<Int>(error)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -74,7 +114,20 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Flowable to Either - if success - return Right`() {
+    fun `Single to Either - if failure - return Left (base mapper)`() {
+        val single = Single.error<Int>(error)
+        val exceptionMapper = object : ExceptionMapper {}
+
+        single.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Left<Failure> }
+            .dispose()
+    }
+
+    @Test
+    fun `Flowable to Either - if success - return Right (lambda mapper)`() {
         val flowable = Flowable.just(3)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -89,7 +142,20 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Flowable to Either - if failure - return Left`() {
+    fun `Flowable to Either - if success - return Right (base mapper)`() {
+        val flowable = Flowable.just(3)
+        val exceptionMapper = object : ExceptionMapper {}
+
+        flowable.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Right<Int> }
+            .dispose()
+    }
+
+    @Test
+    fun `Flowable to Either - if failure - return Left (lambda mapper)`() {
         val flowable = Flowable.error<Int>(error)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -104,7 +170,20 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Completable to Either - if complete - return Right`() {
+    fun `Flowable to Either - if failure - return Left (base mapper)`() {
+        val flowable = Flowable.error<Int>(error)
+        val exceptionMapper = object : ExceptionMapper {}
+
+        flowable.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Left<Failure> }
+            .dispose()
+    }
+
+    @Test
+    fun `Completable to Either - if complete - return Right (lambda mapper)`() {
         val completable = Completable.complete()
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
@@ -119,11 +198,37 @@ class RxEitherTest {
     }
 
     @Test
-    fun `Completable to Either - if failure - return Left`() {
+    fun `Completable to Either - if complete - return Right (base mapper)`() {
+        val completable = Completable.complete()
+        val exceptionMapper = object : ExceptionMapper {}
+
+        completable.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Right<Unit> }
+            .dispose()
+    }
+
+    @Test
+    fun `Completable to Either - if failure - return Left (lambda mapper)`() {
         val completable = Completable.error(error)
         val exceptionMapper: (Throwable) -> Failure = {
             Failure.UnexpectedError
         }
+
+        completable.toEither(exceptionMapper)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { it is Either.Left<Failure> }
+            .dispose()
+    }
+
+    @Test
+    fun `Completable to Either - if failure - return Left (base mapper)`() {
+        val completable = Completable.error(error)
+        val exceptionMapper = object : ExceptionMapper {}
 
         completable.toEither(exceptionMapper)
             .test()
