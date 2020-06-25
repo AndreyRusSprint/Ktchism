9aa8cc9e1f114c11189354abad2537451998227a
absurdpalsy <andreyrussprint@gmail.com>
2020-06-25 13:05:11 +0700

    Core module documentation (#6)
    * Failure docstring
    
    * Exception mapper docstring
    
    * Use case docstring
    
    * Readme. Use case

--- ktchism-core/src/main/java/ktchism/core/mapper/ExceptionMapper.kt
+++ ktchism-core/src/main/java/ktchism/core/mapper/ExceptionMapper.kt
@@ -2,6 +2,17 @@ package ktchism.core.mapper
 
 import ktchism.core.exception.Failure
 
+/**
+ * Base interface for mapper that map errors and exceptions to [Failure].
+ */
 interface ExceptionMapper {
+    /**
+     * Transform error or exception to [Failure].
+     * Return [Failure.UnexpectedError] by default.
+     *
+     * @param from error or exception to be transformed.
+     * @return [Failure].
+     *
+     */
     fun transform(from: Throwable): Failure = Failure.UnexpectedError
 }
